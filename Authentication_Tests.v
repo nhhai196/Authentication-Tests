31af1a4531581bf121d58665e3fa71160072e8f7
hainguyen <hhnguyen@wpi.edu>
2015-03-25 01:38:06 -0400

    State authentication tests

--- Authentication_Tests.v
+++ Authentication_Tests.v
@@ -5,14 +5,31 @@ Definition not_proper_subterm (t:msg) :=
   exists (n': node) (L : msg), 
   t <st L -> t <> L -> r_node n' -> L <[node] n' -> False.
 
-Definition test_component (a t h: msg) (n:node) (k:Key) : Prop :=
-  t = E h k /\ a <st t /\ t <[node] n /\ not_proper_subterm t.
+Definition test_component (a t: msg) (n:node) : Prop :=
+  (exists h k, t = E h k) /\ a <st t /\ t <[node] n /\ not_proper_subterm t.
 
 Definition test (x y : node) (a : msg) : Prop :=
   unique a /\ orig_at x a /\ transformed_edge_for x y a.
 
-Definition incoming_test (x y : node) (a t h: msg) (k:Key) : Prop := 
-  t = E h k /\ ~ (PKeys k) /\ test_component a t h y k.
+Definition incoming_test (x y : node) (a t: msg)  : Prop := 
+  exists h k, t = E h k /\ test x y a /\ ~ PKeys k /\ test_component a t y.
   
-Definition outgoing_test (x y : node) (a t h : msg) (k k' : Key) : Prop :=
-  t = E h k /\ inv k k' /\ ~ (PKeys k') /\ test_component a t h x k.
+Definition outgoing_test (x y : node) (a t : msg) : Prop :=
+  exists h k k', t = E h k /\ test x y a /\ inv k k' /\ ~ PKeys k' /\ test_component a t x.
+
+Section Authentication_tests.
+Variable n n' : node.
+Variable a t: msg.
+Hypothesis incom : incoming_test n n' a t.
+
+Theorem Authentication_test2 : 
+  incoming_test n n' a t ->
+  exists m m', r_node m /\ r_node m' /\ t <[node] m /\
+  transforming_edge_for m m' a.
+Admitted.
+Theorem Authentication_test1 :
+  outgoing_test n n' a t ->
+  exists m m', r_node m /\ r_node m' /\ t <[node] m' /\
+  transforming_edge_for m m' a.
+Admitted.
+End Authentication_tests.  
